506b8d562f83678ead6d3c06943fa51ab4f6b484
engineer846 <54720438+engineer846@users.noreply.github.com>
2023-07-03 18:15:41 +0500

    initial commit

---
+++ Assets/Invector-3rdPersonController/Shooter/Scripts/ThrowSystem/Scripts/vThrowCollectable.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+using System.Collections;
+
+namespace Invector.vCharacterController
+{
+    [vClassHeader("THROW COLLECTABLE", false)]
+    public class vThrowCollectable : vMonoBehaviour
+    {
+        public int amount = 1;
+        public bool destroyAfter = true;
+        //vThrowManager throwManager;
+        protected vThrowManager throwManager;
+
+        public UnityEngine.Events.UnityEvent onCollectObject;
+        public UnityEngine.Events.UnityEvent onReachMaxObjects;
+
+        //private void OnTriggerEnter(Collider other)
+        protected virtual void OnTriggerEnter(Collider other)
+        {
+            if (other.gameObject.CompareTag("Player") && other.GetComponentInChildren<vThrowManager>() != null)
+                throwManager = other.GetComponentInChildren<vThrowManager>();
+        }
+
+        //public void UpdateThrowObj(Rigidbody throwObj)
+        public virtual void UpdateThrowObj(Rigidbody throwObj)
+        {
+            if (throwManager.currentThrowObject < throwManager.maxThrowObjects)
+            {
+                throwManager.SetAmount(amount);
+                throwManager.objectToThrow = throwObj;
+                onCollectObject.Invoke();
+                if (destroyAfter) Destroy(this.gameObject);
+            }
+            else
+            {
+                onReachMaxObjects.Invoke();
+            }
+        }
+    }
+}
